d6d4fda5443352b4aeb77cb8ed6c65636b8a29ed
Labjey <34597669+Labjey@users.noreply.github.com>
2018-07-16 10:44:32 -0500

    missing files and changing the index outcome

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SmartNoteBook_App' });
 });
 
 module.exports = router;
